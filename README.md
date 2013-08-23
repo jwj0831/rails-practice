4c0913cd905850d02d075ddb1a5f3cc2fad3e535
NFM <jwj0831@gmail.com>
2013-08-24 01:06:05 +0900

    first prj commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# For Practicing Ruby on Rails
+* 레일스와 함께하는 애자일 웹 개발 교재 학습을 위한 레일스 프로젝트
+
+## 프로젝트 브랜치 설명
+* zero-iter-a: 6장 학습 애플리케이션 생성
+* zero-iter-b: 6장 DB셋팅 완료, 애플리케이션 실행 가능
+* first-iter: 6장 내용을 실습한 것
+* second-iter: 7장 내용을 실습한 것
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'fe2778a1b67bd8ed6e0cd9dc1894d9451f74525637767020bfbd74f4e9098b4a1d78ff2fe371006b871f5092e4c5eee2839c8c31ceac80f31787fa4220edc8b6'
