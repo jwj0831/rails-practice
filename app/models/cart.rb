0ae1ff3b8763b6651b308525c1669503b7f0bfe4
NFM <jwj0831@gmail.com>
2013-09-14 01:52:30 +0900

    Empty cart function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,7 @@ class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
 
 	def add_product(product_id)
-		current_item = line_items.ind_by_product_id(product_id)
+		current_item = line_items.find_by_product_id(product_id)
 		if current_item
 			current_item.quantity += 1
 		else
@@ -10,5 +10,7 @@ class Cart < ActiveRecord::Base
 		end
 		current_item
 	end
+
+	
 end
 	
